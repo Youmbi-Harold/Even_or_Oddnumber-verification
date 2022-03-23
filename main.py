f28a6b79d182bd8b4a9bf77e68bb92042cbf0a7c
YOUMBI Harold <80352823+Youmbi-Harold@users.noreply.github.com>
2022-03-23 20:32:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#performing the modulo arithmetic
+modulo = number % 2
+#the test condition
+if modulo == 0:
+  print(number, "is an Even number!")
+else:
+  print(number, "is an Odd number!")
+
+
+
